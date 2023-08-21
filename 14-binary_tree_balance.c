ac209103f2708c8d09361689913f40d1569bc6c7
kier-ious <kier.diemer@gmail.com>
2023-08-21 13:17:50 -0700

    cleaned up comments

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -2,10 +2,8 @@
 #include "9-binary_tree_height.c"
 /**
  * binary_tree_balance - measures the balance factor of the BT
- * @tree: a ptr to the root node of the tree to measure
- * the balance factor
- * Return: if tree is NULL, return 0, otherwise
- * return balance factor
+ * @tree: a ptr to the root node of the tree to measure the balance factor
+ * Return: if tree is NULL, return 0, otherwise return balance factor
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
